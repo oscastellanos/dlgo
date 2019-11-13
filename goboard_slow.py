7a44e22a8e2a0049a8965e183ebe4c16673dbf2e
Osvaldo Castellanos <ocstllns@gmail.com>
2019-11-12 22:05:26 -0600

    finished board
    the first iteration of the board class was finalized.

--- goboard_slow.py
+++ goboard_slow.py
@@ -57,7 +57,7 @@ class Board():
         self.num_cols = num_cols
         self._grid = {}
 
-    def place_stones(self, player, point):
+    def place_stone(self, player, point):
         assert self.is_on_grid(point)
         assert self._grid.get(point) is None
         adjacent_same_color = []
@@ -76,6 +76,16 @@ class Board():
                 if neighbor_string not in adjacent_opposite_color:
                     adjacent_opposite_color.append(neighbor_string)
         new_string = GoString(player, [point], liberties)
+
+        for same_color_string in adjacent_same_color:
+            new_string = new_string.merged_with(same_color_string)
+        for new_string_point in new_string.stones:
+            self._grid[new_string_point] = new_string
+        for other_color_string in adjacent_opposite_color:
+            other_color_string.remove_liberty(point)
+        for other_color_string in adjacent_opposite_color:
+            if other_color_string.num_liberties == 0:
+                self._remove_string(other_color_string)
     
     def is_on_grid(self, point):
         return 1 <= point.row <= self.num_rows and \
@@ -92,3 +102,13 @@ class Board():
         if string is None:
             return None
         return string
+    
+    def _remove_string(self, string):
+        for point in string.stones:
+            for neighbor in point.neighbors():
+                neighbor_string = self._grid.get(neighbor)
+                if neighbor_string is None:
+                    continue
+                if neighbor_string is not string:
+                    neighbor_string.add_liberty(point)
+        self._grid[point] = None
